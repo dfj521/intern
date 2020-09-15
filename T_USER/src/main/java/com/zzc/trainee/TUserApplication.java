5d9877ccfdded98e899ee82fae149ba8fb5e0f33
dfj <dfj@qq.com>
2020-09-15 10:56:35 +0800

    dfj -user

---
+++ T_USER/src/main/java/com/zzc/trainee/TUserApplication.java
@@ -0,0 +1,13 @@
+package com.zzc.trainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TUserApplication.class, args);
+	}
+
+}
