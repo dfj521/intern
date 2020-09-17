ca14b034154f20df8da2e14a4523cb118628d0a5
EDZ <l15225808532@163.com>
2020-09-17 09:38:20 +0800

    1

--- T_USER/src/main/java/com/zzc/intern/controller/TraineeDepartmentRelController.java
+++ T_USER/src/main/java/com/zzc/intern/controller/TraineeDepartmentRelController.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.zzc.intern.service.TraineeDepartmentRelService;
+
 /**
  * <p>
  * 实习生&部门 前端控制器
